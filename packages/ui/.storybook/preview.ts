8d889b391ebcfaf47240d74d2bd1d358ae3499e9
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2026-03-04 00:33:47 -0300

    fix: storybook imports

--- packages/ui/.storybook/preview.ts
+++ packages/ui/.storybook/preview.ts
@@ -1,12 +1,13 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { theme } from "@p-f-design/styles";
+import { withThemeFromJSXProvider } from "@storybook/addon-themes";
+import type { Preview } from "storybook-react-rsbuild";
+
+import "@fontsource/material-icons";
 import "@fontsource/public-sans/300.css";
 import "@fontsource/public-sans/400.css";
 import "@fontsource/public-sans/500.css";
 import "@fontsource/public-sans/700.css";
-import "@fontsource/material-icons";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import { withThemeFromJSXProvider } from "@storybook/addon-themes";
-import type { Preview } from "storybook-react-rsbuild";
-import { theme } from "../src/app/styles/theme";
 
 export const decorators = [
 	withThemeFromJSXProvider({
